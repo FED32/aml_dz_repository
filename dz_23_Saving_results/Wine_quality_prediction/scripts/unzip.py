79667e5680159892bebc419b2ef6ca81eb342f2a
FED32 <serega.fed@mail.ru>
2021-05-16 22:51:14 +0300

    add dz_23

---
+++ dz_23_Saving_results/Wine_quality_prediction/scripts/unzip.py
@@ -0,0 +1,3 @@
+import zipfile
+zfile = zipfile.ZipFile('C:/Netology/dz_repository/04_aml/dz_23_Saving_results/Wine_quality_prediction/data/external/archive.zip')
+zfile.extractall('C:/Netology/dz_repository/04_aml/dz_23_Saving_results/Wine_quality_prediction/data/external/')
